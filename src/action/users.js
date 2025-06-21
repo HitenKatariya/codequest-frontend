602a457f4b3c3ea8505d66f6cdcce51b4ac75412
HitenKatariya <148608553+HitenKatariya@users.noreply.github.com>
2025-06-21 12:35:33 +0530

    Update users.js

--- src/action/users.js
+++ src/action/users.js
@@ -1,4 +1,5 @@
 import * as api from "../api"
+import { setcurrentuser } from "./currentuser";
 export const fetchallusers=()=> async(dispatch)=>{
     try {
         const {data}=await api.getallusers();
@@ -12,6 +13,13 @@ export const updateprofile=(id,updatedata)=>async(dispatch)=>{
     try {
         const {data}=await api.updateprofile(id,updatedata);
         dispatch({type:"UPDATE_CURRENT_USER",payload:data});
+        // Update localStorage and Redux current user if this is the logged-in user
+        const profile = JSON.parse(localStorage.getItem("Profile"));
+        if (profile && profile.result && profile.result._id === data._id) {
+            const updatedProfile = { ...profile, result: data };
+            localStorage.setItem("Profile", JSON.stringify(updatedProfile));
+            dispatch(setcurrentuser(updatedProfile));
+        }
     } catch (error) {
         console.log(error)
     }
